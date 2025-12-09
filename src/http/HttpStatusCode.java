877eede3097dfe5885eb3bcb05bad65ab30a672e
yelmach <yassineelmach0@gmail.com>
2025-12-09 14:55:34 +0100

    add http status code enum

--- src/http/HttpStatusCode.java
+++ src/http/HttpStatusCode.java
@@ -1,5 +1,30 @@
 package http;
 
-public class HttpStatusCode {
-    
+public enum HttpStatusCode {
+    OK(200, "OK"),
+    BAD_REQUEST(400, "Bad Request"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    PAYLOAD_TOO_LARGE(413, "Payload Too Large"),
+    URI_TOO_LONG(414, "URI Too Long"),
+    HEADER_FIELDS_TOO_LARGE(431, "Request Header Fields Too Large"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    NOT_IMPLEMENTED(501, "Not Implemented");
+
+    private final int code;
+    private final String reasonPhrase;
+
+    HttpStatusCode(int code, String reasonPhrase) {
+        this.code = code;
+        this.reasonPhrase = reasonPhrase;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getReasonPhrase() {
+        return reasonPhrase;
+    }
 }
