1e055170dad5d3fd6f8492a54a266d453cff4523
yelmach <yassineelmach0@gmail.com>
2025-12-09 17:12:16 +0100

    add http request class

--- src/http/HttpRequest.java
+++ src/http/HttpRequest.java
@@ -1,5 +1,63 @@
 package http;
 
+import java.nio.charset.StandardCharsets;
+
 public class HttpRequest {
-    
+    private HttpMethod method;
+    private String path;
+    private String httpVersion;
+    private HttpHeaders headers;
+    private byte[] body;
+
+    public HttpRequest() {
+        this.headers = new HttpHeaders();
+    }
+
+    public HttpMethod getMethod() {
+        return method;
+    }
+
+    public void setMethod(HttpMethod method) {
+        this.method = method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getHttpVersion() {
+        return httpVersion;
+    }
+
+    public void setHttpVersion(String httpVersion) {
+        this.httpVersion = httpVersion;
+    }
+
+    public HttpHeaders getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(HttpHeaders headers) {
+        this.headers = headers;
+    }
+
+    public void addHeader(String key, String value) {
+        this.headers.add(key, value);
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
+
+    public String getBodyAsString() {
+        return new String(body, StandardCharsets.UTF_8);
+    }
+
+    public void setBody(byte[] body) {
+        this.body = body;
+    }
 }
