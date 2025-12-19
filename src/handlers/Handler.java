86cc8db5f3d91464cee9a90390ce75951a546277
yelmach <yassineelmach0@gmail.com>
2025-12-19 12:45:00 +0100

    restructure the project files

---
+++ src/handlers/Handler.java
@@ -0,0 +1,8 @@
+package handlers;
+
+import http.HttpRequest;
+import http.ResponseBuilder;
+
+public interface Handler {
+    void handle(HttpRequest request, ResponseBuilder response);
+}
--- src/http/IRequestHandler.java
+++
@@ -1,5 +0,0 @@
-package http;
-
-public interface IRequestHandler {
-    void handle(HttpRequest request, HttpResponse response);
-}
